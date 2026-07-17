ec28f0d1baecde623de927037cacf19c8635a4d8
Manahil Nasir <manahilnasir555@gmail.com>
2026-07-17 21:26:58 +0500

    Improve responsiveness

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -9,7 +9,19 @@ const NavBar =()=>{
             Turkify Travels
           </NavLink>
 
-          <div className="collapse navbar-collapse">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <NavLink className="nav-link" to="/">
